f23ebd3d70faad99f8e822bf164f9da21418a4e3
ramanan1079 <ramanan3067@gamil.com>
2022-02-02 11:16:56 +0530

    today next project

---
+++ exframe.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+class exframe extends Frame
+{
+exframe()
+{
+
+Frame frame=new Frame("MY frame");
+
+
+frame.setSize(500,500);
+
+
+
+frame.setLayout(null);
+
+
+frame.setVisible(true);
+
+}
+
+public static void main(String ar[])
+{
+}
+}
